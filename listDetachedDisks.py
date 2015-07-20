4d3f6b85d29687ca71576ed14752a270b786cc4c
Michael Andersen <mandersen@schubergphilis.com>
2015-07-20 11:28:49 +0200

    Refactored listDetachedDisks to use generic parse_arguments

--- listDetachedDisks.py
+++ listDetachedDisks.py
@@ -19,19 +19,23 @@
 #      specific language governing permissions and limitations
 #      under the License.
 
-# Script to search primary storage pools for 'orphaned disks' and remove
+# Script to search primary storage pools for 'detached disks' and remove
 # them to free up space
 
 import sys
 import getopt
 import math
 import os.path
+import random
+import argparse
 
 from cloudstackops import cloudstackops
 from cloudstackops import cloudstackopsssh
 from cloudstackops.cloudstackstorage import StorageHelper
 
-from prettytable import PrettyTable
+from prettytable import PrettyTable 
+
+__version__ = "0.2"
 
 
 def get_volume_filesize(file_uuid_in_cloudstack, *filelist):
@@ -44,78 +48,60 @@ def get_volume_filesize(file_uuid_in_cloudstack, *filelist):
             size = int(filelist[filepath])
     return size
 
-# Function to handle our arguments
+# per script custom arguments 
+def add_custom_arguments(parser, required):
+    # 'required' and 'parser' must be argparse ArgumentParser objects 
+     
+    required.add_argument("-z", "--zone", help="Zone Name", required=True)
+     
+    parser.add_argument("-t", "--cluster", help="Cluster Name")
+    parser.add_argument("-V", "--version", action='version', version="%(prog)s (version " + __version__ + ")")
+    
+    # return parser object so we can parse_args()
+    return parser
 
-
-def handle_arguments(argv):
-    global DEBUG
-    DEBUG = 0
-    global DRYRUN
-    DRYRUN = 1
-    global FORCE
-    FORCE = 0
-    global zone
-    zone = ''
-    global clusterarg
-    clusterarg = ''
-    global configProfileName
-    configProfileName = ''
-
-    # Usage message
-    help = "Usage: " + os.path.basename(__file__) + ' [options] ' + \
-        '\n  --config-profile -c <profilename>\t\tSpecify the CloudMonkey profile name to get the credentials from (or specify in ./config file) [required]' + \
-        '\n  --zone -z <zonename>\t\t\t\tZone Name [required]\t' + \
-        '\n  --cluster -t <clustername>\t\t\tCluster Name [optional]\t' + \
-        '\n  --debug\t\t\t\t\tEnable debug mode [optional]'
-    try:
-        opts, args = getopt.getopt(
-            argv, "hc:z:t:", ["config-profile=", "zone=", "clusterarg=", "debug"])
-
-    except getopt.GetoptError as e:
-        print "Error: " + str(e)
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print help
-            sys.exit()
-        elif opt in ("-c", "--config-profile"):
-            configProfileName = arg
-        elif opt in ("--debug"):
-            DEBUG = 1
-        elif opt in ("-z", "--zone"):
-            zone = arg
-        elif opt in ("-t", "--cluster"):
-            clusterarg = arg
-
-    # Print help if required options not provided
-    if len(configProfileName) == 0 or len(zone) == 0:
-        print help
-        exit(1)
 
 ## MAIN ##
 
 # Parse arguments
 if __name__ == "__main__":
-    handle_arguments(sys.argv[1:])
+    
+    description = "List Detached disks based on Volume Cloudstack api vs Storaaepools"
+    
+    # Init our classes
+    c = cloudstackops.CloudStackOps()
+    cs = cloudstackopsssh.CloudStackOpsSSH()
+    
 
-# Init our classes
-c = cloudstackops.CloudStackOps(DEBUG, DRYRUN, FORCE)
-cs = cloudstackopsssh.CloudStackOpsSSH()
+    parser, required = c.add_generic_arguments(description)
+    try:
+        parser = add_custom_arguments(parser, required)
+    
+    except Exception:
+        pass
+    
+    finally:
+        args = c.parse_arguments(parser)
 
-# make credentials file known to our class
-c.configProfileName = configProfileName
+    # assign custom options to globals
+    zone = args.zone
+    
+    if args.cluster:
+        clusterarg = args.cluster
+        
+    c.configProfileName = args.configprofile
+
 
 # Init the CloudStack API
 c.initCloudStackAPI()
 
-if DEBUG == 1:
+if c.DEBUG == 1:
     print "DEBUG: API address: " + c.apiurl
     print "DEBUG: ApiKey: " + c.apikey
     print "DEBUG: SecretKey: " + c.secretkey
 
 # Check cloudstack IDs
-if DEBUG == 1:
+if c.DEBUG == 1:
     print "DEBUG: Checking CloudStack IDs of provided input.."
 
 zoneid = c.getZoneId(zone)
@@ -126,12 +112,9 @@ if zoneid is None:
     exit(1)
 
 # get all clusters in zone if no cluster is given as input
-if clusterarg is None or clusterarg == '':
-
+if 'clusterarg' not in locals():
     clusters = c.listClusters({'zoneid': zoneid, 'listall': 'true'})
-
 else:
-
     clusters = c.listClusters(
         {'zoneid': zoneid, 'name': clusterarg, 'listall': 'true'})
 
@@ -143,26 +126,27 @@ if clusters is None:
 
 # get a list of storage pools for each cluster
 t_storagepool = PrettyTable(
-    ["Cluster", "Storage Pool", "Number of Orphaned disks", "Real Space used (GB)"])
+    ["Cluster", "Storage Pool", "Number of detached disks", "Real Space used (GB)"])
 
 for cluster in clusters:
     storagepools = []
     storagepools.append(c.getStoragePool(cluster.id))
-    random_hypervisor = c.getHostsFromCluster(cluster.id).pop()
+    random_hypervisor = random.choice(c.getHostsFromCluster(cluster.id))
+    
     # flatten storagepool list
     storagepools = [y for x in storagepools for y in x]
 
     # # if there are storage pools (should be)
     if len(storagepools) > 0:
 
-        storagehelper = StorageHelper(debug=DEBUG)
+        storagehelper = StorageHelper(debug=c.DEBUG)
 
         for storagepool in storagepools:
             used_space = 0
 
-            # Get list of orphaned cloudstack disks for storagepool
-            print "[INFO]: Retrieving list of orphans for storage pool", storagepool.name
-            orphans = c.getDetachedVolumes(storagepool.id)
+            # Get list of detached_disks from cloudstack for storagepool
+            print "[INFO]: Retrieving list of detached disks for storage pool", storagepool.name
+            detached_disks = c.getDetachedVolumes(storagepool.id)
 
             storagepool_devicepath = storagepool.ipaddress + \
                 ":" + str(storagepool.path)
@@ -173,7 +157,8 @@ for cluster in clusters:
                 random_hypervisor.ipaddress, storagepool_devicepath)
 
             if primary_mountpoint is None:
-                print "[DEBUG]: no physical volume list retrieved for " + storagepool.name + " skipping"
+                if c.DEBUG == 1:
+                    print "[DEBUG]: no physical volume list retrieved for " + storagepool.name + " skipping"
                 storagepool_filelist = None
 
             else:
@@ -181,39 +166,39 @@ for cluster in clusters:
                     random_hypervisor.ipaddress, primary_mountpoint)
 
             t = PrettyTable(["Domain", "Account", "Name", "Cluster", "Storagepool", "Path",
-                             "Allocated Size (GB)", "Real Size (GB)", "Orphaned"])
+                             "Allocated Size (GB)", "Real Size (GB)", "Disk Found"])
 
-            for orphan in orphans:
-                isorphaned = ''
+            for detached_disk in detached_disks:
+                diskfound = ''
 
-                orphan_allocated_sizeGB = (orphan.size / math.pow(1024, 3))
+                disk_allocated_sizeGB = (detached_disk.size / math.pow(1024, 3))
 
                 if storagepool_filelist is None:
-                    orphan_real_sizeGB = 'n/a'
-                    isorphaned = '?'
+                    disk_real_sizeGB = 'n/a'
+                    diskfound = '?'
 
                 else:
-                    orphan_real_sizeGB = get_volume_filesize(
-                        orphan.path, storagepool_filelist)
+                    disk_real_sizeGB = get_volume_filesize(
+                        detached_disk.path, storagepool_filelist)
 
-                    if orphan_real_sizeGB is not None:
-                        used_space += (orphan_real_sizeGB / 1024)
-                        orphan_real_sizeGB = format(
-                            (orphan_real_sizeGB / 1024), '.2f')
-                        isorphaned = 'Y'
+                    if disk_real_sizeGB is not None:
+                        used_space += (disk_real_sizeGB / 1024)
+                        disk_real_sizeGB = format(
+                            (disk_real_sizeGB / 1024), '.2f')
+                        diskfound = 'Y'
 
                     else:
-                        orphan_real_sizeGB = 0
-                        isorphaned = 'N'
+                        disk_real_sizeGB = 0
+                        diskfound = 'N'
 
-                # add a row with orphan details
-                t.add_row([orphan.domain, orphan.account, orphan.name, cluster.name, storagepool.name, orphan.path,
-                           orphan_allocated_sizeGB, orphan_real_sizeGB, isorphaned])
+                # add a row with disk details
+                t.add_row([detached_disk.domain, detached_disk.account, detached_disk.name, cluster.name, storagepool.name, detached_disk.path,
+                           disk_allocated_sizeGB, disk_real_sizeGB, diskfound])
 
-            # Print orphan table
+            # Print disk table
             print t.get_string()
             t_storagepool.add_row(
-                [cluster.name, storagepool.name, len(orphans), format(used_space, '.2f')])
+                [cluster.name, storagepool.name, len(detached_disks), format(used_space, '.2f')])
 
 print "Storagepool Totals"
 print t_storagepool.get_string()
